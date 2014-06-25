dc4928ec11e6d2e693ba3f48e37d1473b7fcc84f
Roy Zheng <roycraft3@gmail.com>
2014-06-25 11:13:41 -0700

    config verification

--- src/classes/configverify.php
+++ src/classes/configverify.php
@@ -1,11 +1,50 @@
 <?php
+function verifyConfig(){
 
-	      'PIN_LENGTH'=>6,         //length of pin perpendicular to chip
-	      'PIN_WIDTH'=>18,       //length of pin parallel to chip
-	      'PIN_SPACING'=>6,       //space between pins
-	      'MIN_CPU_PIN_EDGE'=>3,  //minimum distance between edge of CPU and a pin
-	      'MIN_CHIP_PIN_EDGE'=>3, //minimum distance between edge of generic chip and a pin
-	      'PIN_STROKEWIDTH'=>.5,  //thickness of the pin lines
-	      'MAX_COMMAND_LENGTH'=>200,//maximum argument length to convert; lower this if some commands are not going through
+  $exists=function(){
+    $mandatory=array(
+		     'PIN_LENGTH',
+		     'PIN_WIDTH',
+		     'MIN_CPU_PIN_EDGE',
+		     'MIN_CHIP_PIN_EDGE',
+		     'PIN_STROKEWIDTH',
+		     'MAX_COMMAND_LENGTH',
+		     );
+    foreach($mandatory as $v){
+      if(!in_array($v,$GLOBALS['config']))
+	return false;
+    }
+  }
+  $exists();
 
+  $is_integer=function($v){
+    return is_integer($v);
+  };
+  $is_greater_than_zero=function($v){
+    return $v > 0;
+  };
+  $is_geq_zero=function($v){
+    return $v >= 0;
+  }
+  $integer_greater_than_zero=function($v){
+    return (is_integer($v)&&$v>0);
+  };
+  $float_greater_than_zero=function($v){
+    return (is_numeric($v) && $v >= 0);
+  }
+  $valueVerify=array(
+		     'PIN_LENGTH'=>$integer_greater_than_zero,
+		     'PIN_WIDTH'=>$integer_greater_than_zero,
+		     'MIN_CPU_PIN_EDGE'=>$is_geq_zero,
+		     'MIN_CHIP_PIN_EDGE'=$is_geq_zero,
+		     'PIN_STROKEWIDTH'=>$float_greater_than_zero,
+		     'MAX_COMMAND_LENGTH'=>$integer_greater_than_zero,
+		     );
+  
+  foreach($valueVerify as $verify){
+    if(!$verify[1]($GLOBALS['config'][$verify[0]]))
+      return false;
+  }
+  return true;
+}
 ?>
---
+++ tests/classes/ConfigVerifyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class ConfigVerifyTest extends PHPUnit_Framework_Testcase{
+
+  public function setUp(){
+    $GLOBALS['config']=array( );
+  }
+  public function testGood(){
+
+  }
+  public function testMissing(){
+
+  }
+  public function testBadValue(){
+
+  }
+}
+
+?>
